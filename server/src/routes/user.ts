9c0e92f221e08893fa29421eb50fffa527ad3744
jogeshgupta963 <jogeshgupta963@gmail.com>
2023-08-23 12:06:32 +0530

    feat: :sparkles: user auth

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,3 +1,7 @@
 import express from "express";
+import { signup, login } from "../controllers/user";
 
 export const router = express.Router();
+
+router.route("/login").post(login);
+router.route("/signup").post(signup);
